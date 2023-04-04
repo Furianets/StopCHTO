9a0ace108b7a81dfaa8bd6b8add6b07217b90099
Furianets <furianets94@gmail.com>
2023-04-04 23:42:58 +0400

    //third number. Fibanacci=34

---
+++ Fibba34.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int fib_helper(int a, int b, int n) {
+    if (n == 0) {
+        return a;
+    }
+    else {
+        return fib_helper(b, a + b, n - 1);
+    }
+}
+
+int fib(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    else {
+        return fib_helper(0, 1, n - 1);
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Enter n" << std::endl;
+    std::cin >> n;
+    std::cout << fib(n) << std::endl;
+    return 0;
+}
