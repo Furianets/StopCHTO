7b60d02ce2631f9a3731d566210269e919307765
Furianets <129891980+Furianets@users.noreply.github.com>
2023-04-05 00:27:54 +0400

    Update Fibba34.cpp

--- Fibba34.cpp
+++ Fibba34.cpp
@@ -1,27 +1,24 @@
-﻿#include <iostream>
-
-int fib_helper(int a, int b, int n) {
-    if (n == 0) {
-        return a;
-    }
-    else {
-        return fib_helper(b, a + b, n - 1);
-    }
-}
-
+#include <iostream>
 int fib(int n) {
-    if (n <= 0) {
+    if (n == 0) {
         return 0;
     }
-    else {
-        return fib_helper(0, 1, n - 1);
+    if (n == 1) {
+        return 1;
     }
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main() {
     int n;
-    std::cout << "Enter n" << std::endl;
+    std::cout << "Enter a number n: ";
     std::cin >> n;
-    std::cout << fib(n) << std::endl;
+
+    std::cout << "First " << n << " Fibonacci numbers:" << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        std::cout << fib(i) << std::endl;
+    }
+
     return 0;
 }
